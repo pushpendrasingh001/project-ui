d99001d7d8495f0062bc5f1950afe36bb77b40ab
pushpendrasingh001 <pushpendrasingh1919@gmail.com>
2024-05-10 14:46:26 +0530

    update

--- src/components/Achievment.jsx
+++ src/components/Achievment.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Achievment() {
   return (
-    <div className='p-[100px]'>
+    <div className='p-[100px] bg-slate-300'>
     
     <div className='flex justify-center text-5xl font-bold font mb-10'>
   <p>ACHIEVE<span className='text-red-400'>MENTS</span></p>
@@ -26,7 +26,9 @@ function Achievment() {
   </li>
   <li>
     <hr/>
-    <div className="timeline-start timeline-box">isought to enhance transparency, security, and immutability by harnessing the power of blockchain technology. By successfully conducting elections in our college using this innovative system, we not only demonstrated its effectiveness but also earned the distinguished honor of having our achievement registered in the India Book of Records as the first-ever blockchain-based election conducted in India.Pod</div>
+    <div className="timeline-start timeline-box">
+      <img src="../../public/CUH-Coding-Club-5-8-2024(4).png" alt="" />
+    </div>
     <div className="timeline-middle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
     </div>
@@ -46,6 +48,16 @@ function Achievment() {
     <div className="timeline-middle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
     </div>
+  </li>
+  <li>
+    <hr/>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-end timeline-box">
+      <img src="../../public/CUH-Coding-Club-5-8-2024(3).png" alt="" />
+    </div>
+    <hr/>
   </li>
 </ul>
     </div>
